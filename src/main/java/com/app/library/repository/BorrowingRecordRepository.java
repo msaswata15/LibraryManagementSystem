b022a321da96ab1cbb1c24be1d7d8dc38c224df9
msaswata15 <msaswata15@gmail.com>
2025-08-25 10:08:52 +0530

    Refactor import statements in BorrowingRecordRepository for improved organization

--- src/main/java/com/app/library/repository/BorrowingRecordRepository.java
+++ src/main/java/com/app/library/repository/BorrowingRecordRepository.java
@@ -1,13 +1,13 @@
 package com.app.library.repository;
 
-import com.app.library.model.BorrowingRecord;
-import com.app.library.model.Book;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.app.library.model.BorrowingRecord;
 
 @Repository
 public interface BorrowingRecordRepository extends JpaRepository<BorrowingRecord, Long> {
